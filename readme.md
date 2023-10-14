90d05298aa4f399d2b1a4fa9f822f2beed6cd8ed
HiChon <59905927+chon26909@users.noreply.github.com>
2023-10-14 22:08:40 +0700

    create project

---
+++ readme.md
@@ -0,0 +1,80 @@
+## Typescript 101 by HiChon
+
+### Craete Folder
+
+```
+mkdir typescript-101 && cd typescript-101
+```
+
+### Init Project
+
+```
+npm init -y
+```
+
+### Install typescript
+
+```
+npm install -D typescript ts-node
+```
+
+> --save-dev หรือ -D
+> แพ็กเกจจะปรากฎใน devDependencies ในไฟล์ package.json
+> การใช้ :
+> เพื่อบันทึกแพ็กเกจที่จำเป็นต่อการพัฒนาแอปพลิเคชั่น หรือเพื่อใช้เฉพาะใน develop environment เท่านั้น เช่น แพ็กเกจที่ใช้สำหรับทำการทดสอบ (unit test) หรือแพ็กเกจสำหรับการลดขนาดหรือคอมไพล์ไฟล์ เป็นต้น
+
+### Init Typescript config
+
+```
+npm tsc --init
+```
+
+edit file tsconfig.json
+
+"target": "ES2022"
+
+> "rootDir": "./src"
+>
+> "outDir": "./dist"
+
+### Install nodemon
+
+```
+npm install -D nodemon
+```
+
+create file nodemon.json at root directory
+
+```
+{
+  "watch": ["src"],
+  "ext": "ts",
+  "ignore": ["src/**/*.spec.ts"],
+  "exec": "ts-node ./src/index.ts"
+}
+```
+
+edit file package.json
+
+>
+
+```
+"scripts": {
+  "dev": "nodemon",
+  "build": "tsc --project ./"
+},
+```
+
+### Create code
+
+create file ./src/index.ts
+
+```
+console.log('Hello world!')
+```
+
+### Run code
+
+```
+npm run dev
+```
---
+++ src/index.ts
@@ -0,0 +1,118 @@
+console.clear();
+
+// variables scope
+var a = "Somchai";
+let b = "b";
+const c = "c";
+
+b = a;
+
+// console.log("a=", a, "b=", b, "c  =", c);
+
+// let username = "Somchai";
+
+// const exampleA = function () {
+//   console.log(username);
+// };
+
+// // function expression
+// const exampleB = () => {
+//   console.log("example");
+// };
+
+// exampleB();
+
+// type
+type Person = {
+  name: string;
+};
+
+// interface
+interface ICheckOlderTeen {
+  age: number;
+  older: boolean;
+}
+
+const checkOlderTeen = (age: number): ICheckOlderTeen => {
+  if (age > 25) {
+    return {
+      age: age,
+      older: true,
+    };
+  }
+
+  return {
+    age: age,
+    older: false,
+  };
+};
+
+console.log("checkOlder", checkOlderTeen(30));
+
+interface IAnimal {
+  type: string;
+  age?: number;
+}
+
+interface ICat extends IAnimal {
+  name: string;
+  owner?: string;
+}
+
+const cat: ICat = {
+  name: "max",
+  age: 10,
+  type: "เลี้ยงลูกด้วยนม",
+};
+
+interface IOwner {
+  firstname: string;
+  lastname: string;
+  cats: ICat[];
+}
+
+const cats: ICat[] = [
+  {
+    name: "Mali",
+    age: 9,
+    type: "เลี้ยงลูกด้วยนม",
+  },
+  {
+    name: "Poa",
+    age: 7,
+    type: "เลี้ยงลูกด้วยนม",
+  },
+];
+
+const ownerCat: IOwner[] = [
+  {
+    firstname: "Chalee",
+    lastname: "Champ",
+    cats: cats,
+  },
+];
+
+console.log("ownerCat", JSON.stringify(ownerCat, null, 5));
+
+//const totalCate = ownerCat[0].cats.length()
+
+const countMyCat = (data: IOwner[]) => {
+  const result = data.map((row: IOwner) => {
+    return {
+      name: row.firstname,
+      count_cat: row.cats.length,
+      cat_name: row.cats.map((cat) => cat.name),
+      cat_name2: row.cats.map(({ name, type }) => ({ name, type })),
+      cat_name3: row.cats.map(({ name, type }) => {
+        return {
+          name,
+          type,
+        };
+      }),
+    };
+  });
+
+  return result;
+};
+
+console.log("countMyCat", JSON.stringify(countMyCat(ownerCat), null, 5));
